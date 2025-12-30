91e257dafb2b7f4b64accaa87ba773452e89391a
Alan Jose Santo <realityaudiostudio@gmail.com>
2025-12-30 22:23:22 +0530

    Minor bug

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // REPLACE THESE WITH YOUR KEYS FROM SUPABASE DASHBOARD
-const supabaseUrl = 'https://gavzxskqwdtnfvdxmfjk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdhdnp4c2txd2R0bmZ2ZHhtZmprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwODUxMzUsImV4cCI6MjA4MjY2MTEzNX0.SEvSxAChXTSzPLZIjOs86hWRCKPnOx8isrzqP0X5y0E'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
